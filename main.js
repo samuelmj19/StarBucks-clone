2d66a549869b08fab28394502d9aa548d21b4d12
Samuel Morales <samuelmj1999@gamil.com>
2022-05-19 02:42:18 -0400

    menu resposive with JS

---
+++ main.js
@@ -0,0 +1,7 @@
+
+function showMenu(){
+    const menu = document.querySelector('.nav--responsive-menu_hidden').classList.toggle('active')
+}
+function showFooterInfo(){
+    const info =document.querySelector('.hidden-info--button').classList.toggle('active')
+}
